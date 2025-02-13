a860559eec4cc2cc0d7be0ba078ca10792ee62f4
thialms <thiiago.allmeida44@gmail.com>
2025-02-13 01:24:12 -0300

    deploy-to-test

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
     <div>
 
       {
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <BrowserRouter basename="/Taskiando/">
           <Routes>
             <Route path="/" element={hasUser ? <Navigate to="/tasks" replace /> : <LoginPage onSubmit={setUser} />} />
             <Route path="/tasks" element={hasUser ? <MainPage name={user} tasks={tasks} onLogout={handleLogout}/> : <Navigate to="/" replace />} />
